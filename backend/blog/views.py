96e72cef5d77c5f87be38b33802563b4aa73b203
huhuhu <163014042+ssssswwwwwxxxxxxxwaxaca@users.noreply.github.com>
2025-07-31 17:06:29 +0800

    feat(blog): 添加文章详情接口和相关功能
    - 在 models.py 中添加了 Post 模型的 updated_at 字段和 __str__ 方法
    - 在 serializers.py 中更新了 PostSerializer，增加了只读字段
    - 在 views.py 中添加了 perform_create、perform_update 和 details 方法
    - 更新了前端路由，增加了新的页面组件

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Post
 from .serializers import PostSerializer
 
@@ -8,3 +10,19 @@ from .serializers import PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
+    permission_classes=[permissions.AllowAny]
+    
+    def perform_create(self, serializer):
+        return serializer.save()
+   
+    def perform_update(self, serializer):
+        return serializer.save()
+   
+    @action(detail=True,methods=['get'])
+    def details(self,request,pk=None):
+        post=self.get_object()
+        serializer=PostSerializer(post)
+        return Response(serializer.data)
+    
+    
+    
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,10 +2,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PostList from '../components/PostList.vue'
 import PostDetail from '../components/PostDetail.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+import PostForm from '../components/PostForm.vue'
+import PostDetail from '../components/PostDetail.vue'
+
+
 
 const routes = [
   { path: '/', component: PostList },
-  { path: '/post/:id', component: PostDetail, props: true }
+  { path: '/post/:id', component: PostDetail, props: true },
+  { path: '/hello', component: HelloWorld },
+  { path: '/post-form', component: PostForm },
+  { path: '/post-form/:id', component: PostForm, props: true },
+    // 添加其他路由配置
+    { path: '/post-form/:id', component: PostForm, props: true },
 ]
 
 const router = createRouter({
